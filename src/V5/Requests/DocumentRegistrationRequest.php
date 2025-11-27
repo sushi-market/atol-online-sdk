9dc23604678c5781c93f82d868cf025ab63f26e2
m0nclous <m0nclous@vk.com>
2025-11-27 21:29:21 +0600

    DocumentRegistrationRequest

---
+++ src/V5/Requests/DocumentRegistrationRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DF\AtolOnline\V5\Requests;
+
+use DF\AtolOnline\Enums\HttpAuthType;
+use DF\AtolOnline\Enums\HttpMethod;
+use DF\AtolOnline\Interfaces\ApiRequestInterface;
+use DF\AtolOnline\V5\DTO\DocumentRegistration\DocumentRegistrationRequestDTO;
+use DF\AtolOnline\V5\Enums\Operation;
+
+final readonly class DocumentRegistrationRequest implements ApiRequestInterface
+{
+    public function __construct(
+        private string $groupCode,
+        private Operation $operation,
+        private DocumentRegistrationRequestDTO $requestDTO,
+    ) {}
+
+    public function getUri(): string
+    {
+        return "$this->groupCode/{$this->operation->value}";
+    }
+
+    public function getMethod(): HttpMethod
+    {
+        return HttpMethod::POST;
+    }
+
+    public function getAuthType(): HttpAuthType
+    {
+        return HttpAuthType::API_KEY;
+    }
+
+    public function getQuery(): ?string
+    {
+        return null;
+    }
+
+    public function getBody(): ?array
+    {
+        return (array) $this->requestDTO;
+    }
+
+    public function getHeaders(): array
+    {
+        return [];
+    }
+}
